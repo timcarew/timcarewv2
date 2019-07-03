65399778bedb8957c4117867c1404967f8771948
Tim Carew <mail@timcarew.com>
2019-07-03 21:26:44 -0230

    finish social drawer

--- src/components/SocialDrawer/SocialDrawer.js
+++ src/components/SocialDrawer/SocialDrawer.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
+import resume from "../../resume.pdf";
 import "./SocialDrawer.css";
 
 class SocialDrawer extends Component {
   render() {
     return(
       <div className="SocialDrawer">
-        <h6 className="SocialDrawer-link">GitHub</h6>
-        <h6 className="SocialDrawer-link">Email</h6>
-        <h6 className="SocialDrawer-link">Resume</h6>
+        <a href="mailto:mail@timcarew.com" className="SocialDrawer-link">Email</a>
+        <a href={resume} className="SocialDrawer-link" target="_blank" rel="noopener noreferrer">Resume</a>
+        <a href="https://github.com/timcarew" target="_blank" rel="noopener noreferrer" className="SocialDrawer-link" id="SocialDrawer-link-dynamic"><span>@</span></a>
       </div>
     );
   }
