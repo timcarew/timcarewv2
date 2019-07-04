c2ee58d4b92d1717979d8320c177f382accfe21d
Tim Carew <mail@timcarew.com>
2019-07-04 13:35:30 -0230

    hover functionality - broswer recog

--- src/App.js
+++ src/App.js
@@ -1,27 +1,76 @@
 import React, { Component } from 'react';
 import Computer from "./components/Computer/Computer";
 import SocialDrawer from "./components/SocialDrawer/SocialDrawer";
+import { debounce } from 'debounce';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: ""
+      message: "Hello, World!",
+      page: "home"
     };
   }
+
+  changePage = (page) => {
+    this.setState({ page: page});
+  }
+
+  typeMessage = debounce((message) => {
+    // erase old message
+    this.setState({ message: ""});
+
+    // type new one
+    let interval = setInterval(() => {
+      // get first character
+      let characterToAdd = message.slice(0, 1);
+      // pop first character
+      message = message.substr(1, message.length);
+      if (message) {
+        let messageToType = this.state.message + characterToAdd;
+        this.setState({ message: messageToType });
+      }
+      else {
+        let messageToType = this.state.message + characterToAdd;
+        this.setState({ message: messageToType });
+        clearInterval(interval);
+      }
+    }, 50);
+  }, 1000, true);
+
   render() {
     return (
       <div className="App">
-        <div className="App-name">
-          <div className="App-name-text">
-            <h2 className="App-name-text-h2">Tim Carew</h2>
+      {
+        this.state.page === "home" ? 
+        // render homepage
+        <div className="App-Main">
+          <div className="App-name">
+            <div className="App-name-text">
+              <h2 className="App-name-text-h2">Tim Carew</h2>
+            </div>
+          </div>
+          <div className="App-Computer">
+            <h2 onMouseEnter={() => this.typeMessage("cd ./about")} onClick={() => this.changePage("about")} className="App-Computer-link App-Computer-about">About Me</h2>
+            <Computer message={this.state.message} />
+            <h2 onMouseEnter={() => this.typeMessage("cd ./projects")} onClick={() => this.changePage("projects")} className="App-Computer-link App-Computer-projects">Projects</h2>
+          </div>
+          <div className="App-links">
+            <SocialDrawer typeMessage={this.typeMessage}/>
           </div>
         </div>
-        <Computer message={this.state.message} />
-        <div className="App-links">
-          <SocialDrawer />
-        </div>
+        
+        : this.state.page === "about" ? 
+        // render about page
+        <h2>Welcome To About</h2>
+
+        : this.state.page === "projects" ?
+        // render projects page
+        <h2>Welcome To Projects</h2>
+
+        : false
+      }
       </div>
     );
   }
--- src/components/SocialDrawer/SocialDrawer.js
+++ src/components/SocialDrawer/SocialDrawer.js
@@ -4,11 +4,12 @@ import "./SocialDrawer.css";
 
 class SocialDrawer extends Component {
   render() {
+    let browser = "chrome";
     return(
       <div className="SocialDrawer">
-        <a href="mailto:mail@timcarew.com" className="SocialDrawer-link">Email</a>
-        <a href={resume} className="SocialDrawer-link" target="_blank" rel="noopener noreferrer">Resume</a>
-        <a href="https://github.com/timcarew" target="_blank" rel="noopener noreferrer" className="SocialDrawer-link" id="SocialDrawer-link-dynamic"><span>@</span></a>
+        <a onMouseEnter={() => this.props.typeMessage("mail -s \"Nice Site!\" mail@timcarew.com")} href="mailto:mail@timcarew.com" className="SocialDrawer-link">Email</a>
+        <a onMouseEnter={() => this.props.typeMessage("open ./resume.pdf")} href={resume} className="SocialDrawer-link" target="_blank" rel="noopener noreferrer">Resume</a>
+        <a onMouseEnter={() => this.props.typeMessage(`${browser} https://timcarew.github.io`)} href="https://github.com/timcarew" target="_blank" rel="noopener noreferrer" className="SocialDrawer-link" id="SocialDrawer-link-dynamic"><span>@</span></a>
       </div>
     );
   }
